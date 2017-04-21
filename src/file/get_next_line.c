6e31401a30759717bde5af27dbeba7eaff6dff2e
aoll <agollivier@gmail.com>
2017-04-21 15:17:05 +0200

    norme free

--- src/file/get_next_line.c
+++ src/file/get_next_line.c
@@ -6,13 +6,47 @@
 /*   By: aollivie <aollivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/10 15:05:03 by aollivie          #+#    #+#             */
-/*   Updated: 2017/02/06 16:08:33 by alex             ###   ########.fr       */
+/*   Updated: 2017/04/21 15:16:13 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_mycpy(char **stat, char **line)
+static int	ft_free_strs(char **s1, char **s2, char **s3, char **s4)
+{
+	if (s1 && *s1)
+	{
+		free(*s1);
+		*s1 = NULL;
+	}
+	if (s2 && *s2)
+	{
+		free(*s2);
+		*s2 = NULL;
+	}
+	if (s3 && *s3)
+	{
+		free(*s3);
+		*s3 = NULL;
+	}
+	if (s4 && *s4)
+	{
+		free(*s4);
+		*s4 = NULL;
+	}
+	return (EXIT_SUCCESS);
+}
+
+static int	ft_mycpy_tp(char **line, char **stat, char **tp, int l)
+{
+	ft_free_strs(line, stat, NULL, NULL);
+	*line = ft_strnew(0);
+	*stat = ft_strsub((char *)*tp, 1, l - 1);
+	ft_free_strs(tp, NULL, NULL, NULL);
+	return (1);
+}
+
+static int		ft_mycpy(char **stat, char **line)
 {
 	size_t	i;
 	size_t	l;
@@ -22,84 +56,67 @@ int		ft_mycpy(char **stat, char **line)
 	i = 0;
 	l = ft_strlen(tp);
 	if (*stat[0] == '\n' || *stat[0] == '\0')
-	{
-		free(*line);
-		*line = ft_strnew(0);
-		free(*stat);
-		*stat = ft_strsub((char *)tp, 1, l - 1);
-		free(tp);
-		tp = NULL;
-		return (1);
-	}
+		return (ft_mycpy_tp(line, stat, &tp, l));
 	while (tp[i] != '\n' && tp[i] != '\0')
 		i++;
 	if (tp[i] == '\n')
 	{
-		free(*stat);
-		free(*line);
+		ft_free_strs(line, stat, NULL, NULL);
 		*line = ft_strsub((char *)tp, 0, i);
 		*stat = ft_strsub((char *)tp, i + 1, l - i - 1);
-		free(tp);
-		tp = NULL;
+		ft_free_strs(&tp, NULL, NULL, NULL);
 		return (1);
 	}
-	free(tp);
-	tp = NULL;
+	ft_free_strs(&tp, NULL, NULL, NULL);
 	return (0);
 }
 
-int		get_next_line(int const fd, char **line)
+static int	ft_get_loop(int const fd, char **line , char **stat)
 {
 	char		*buf;
-	static char *stat;
-	char *tmp;
+	char 		*tmp;
 	int			rd;
 
-	*line = ft_strnew(0);
 	buf = ft_strnew(BUFF_SIZE);
-	if (fd < 0)
-		return (-1);
-	if (stat && (rd = ft_mycpy(&stat, line)) > 0)
-	{
-		free(buf);
-		buf = NULL;
-		return (1);
-	}
 	while ((rd = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[rd] = '\0';
-		tmp = ft_strjoin((const char *)stat, (const char *)buf);
-		free(stat);
-		stat = NULL;
-		stat = ft_strdup(tmp);
+		tmp = ft_strjoin((const char *)*stat, (const char *)buf);
+		free(*stat);
+		*stat = NULL;
+		*stat = ft_strdup(tmp);
 		free(tmp);
-		tmp = NULL;
-		if (ft_mycpy(&stat, line) > 0)
+		if (ft_mycpy(stat, line) > 0)
 		{
 			free(buf);
-			buf = NULL;
-			free(stat);
-			stat = NULL;
+			free(*stat);
+			*stat = NULL;
 			return (1);
 		}
 	}
+	free(buf);
+	return (EXIT_SUCCESS);
+}
+
+int				get_next_line(int const fd, char **line)
+{
+	static char *stat;
+	int			rd;
+
+	*line = ft_strnew(0);
+	if (fd < 0)
+		return (-1);
+	if (stat && (rd = ft_mycpy(&stat, line)) > 0)
+		return (1);
+	if (ft_get_loop(fd, line, &stat))
+		return (1);
 	if (rd == 0)
-	{
-		free(*line);
-		*line = NULL;
-		free(buf);
-		buf = NULL;
-		free(stat);
-		stat = NULL;
-		return (0);
-	}
+		return (ft_free_strs(line, &stat, NULL, NULL));
 	if (stat)
 	{
 		*line = ft_strdup(stat);
 		free(stat);
 		stat = NULL;
-		free(buf);
-		buf = NULL;
 		return (1);
 	}
 	return (0);
