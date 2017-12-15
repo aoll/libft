07a10658709a3edec2b47e175f74df30968c9866
Alexandre OLLIVIER <aollivie@e1r10p7.42.fr>
2017-12-15 14:21:11 +0100

    check ft_strncmp

--- src/str/ft_strncmp.c
+++ src/str/ft_strncmp.c
@@ -14,9 +14,10 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t				i;
+	size_t					i;
 	unsigned char		*str1;
 	unsigned char		*str2;
+	int							tmp2;
 
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
@@ -26,6 +27,6 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	if (str1 && str2)
 		while (str1[i] && str2[i] && str1[i] == str2[i] && i + 1 < n)
 			i++;
-	return (str1[i] - str2[i]);
-	return (0);
+	tmp2 = str2[i] ? str2[i] : 0;
+	return ((str1[i] ? str1[i] : 0) - tmp2);
 }
