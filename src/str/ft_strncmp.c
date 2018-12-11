c31f072e21ab54ae82d26a4a76ec09581516670a
Alexandre OLLIVIER <aollivie@e1r2p7.42.fr>
2018-12-11 17:27:58 +0100

    42 norme

--- src/str/ft_strncmp.c
+++ src/str/ft_strncmp.c
@@ -14,10 +14,10 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t					i;
+	size_t				i;
 	unsigned char		*str1;
 	unsigned char		*str2;
-	int							tmp2;
+	int					tmp2;
 
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
